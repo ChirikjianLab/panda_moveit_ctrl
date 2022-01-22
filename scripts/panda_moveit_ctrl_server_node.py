5fccddcd5b9b8cf569c52380604f22d2f92e8edf
Hongtao Wu <mengxin@u.nus.edu>
2022-01-22 11:14:50 +0800

    update speed and ee

--- scripts/panda_moveit_ctrl_server_node.py
+++ scripts/panda_moveit_ctrl_server_node.py
@@ -10,10 +10,10 @@ from panda_moveit_ctrl.panda_robot_service import PandaRobotService
 if __name__ == "__main__":
     rospy.init_node("panda_robot_service")
 
-    vel = 0.4
-    acc = 0.4
+    vel = 0.3
+    acc = 0.3
     load_gripper = True
-    ee = "panda_hand"
+    ee = "panda_link8"
     PRS = PandaRobotService(vel, acc, ee=ee, load_gripper=load_gripper)
 
     
